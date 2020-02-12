d357323140f5623a45fb7ba5e0cac955ba729cf2
Wade <liwei@biconomy.com>
2020-02-12 20:45:50 +0800

    add transfer for firt interface

--- operations/interfaces.go
+++ operations/interfaces.go
@@ -6,6 +6,7 @@ import (
 )
 
 type TezosOperationsService interface {
+	CreateBatchPaymentForFirstSend(payments []delegate.Payment, wallet account.Wallet, paymentFee int, gasLimit int, batchSize int) ([]string, error)
 	CreateBatchPayment(payments []delegate.Payment, wallet account.Wallet, paymentFee int, gaslimit int, batchSize int) ([]string, error)
 	InjectOperation(op string) ([]byte, error)
 	GetBlockOperationHashes(id interface{}) ([]string, error)
